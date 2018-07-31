73f2a4de2d5db90afce97744cd6dc0b900b250ab
kazukisona <kazuki@Kazuki-MacBook-Pro.local>
2018-07-31 15:45:10 -0400

    updated all files

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/kazuki/Dropbox/Public/Hunter/Summer2018/Capstone/wine-recommendation/env/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- project/winesite/urls.py
+++ project/winesite/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('api/wine/', views.WineListCreate.as_view()),
     path('api/user/', views.UserListCreate.as_view()),
     path('api/wines/', views.WineTableListCreate.as_view()),
-    path('wine_list/', views.wine_list, name='wine_list')
+    path('wine_list/', views.wine_list, name='wine_list'),
+    path('recommendation/', views.recommendation, name='recommendation'),
+    path('selector/', views.selector, name='selector'),
 ]
