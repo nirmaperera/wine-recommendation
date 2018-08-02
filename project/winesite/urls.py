f18f12c3d2d66aef18c73f6706e828125fa363dd
RatulAhmed <ratulahmed1@gmail.com>
2018-08-02 18:30:33 -0400

    Edited views urls and signup template

--- project/winesite/urls.py
+++ project/winesite/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('recommendation/', views.recommendation, name='recommendation'),
     path('selector/', views.selector, name='selector'),
     path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('newuser', views.new_user, name='newuser'),
 ]
